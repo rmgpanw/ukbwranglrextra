2a3084988770285fb3a3264caecd77493727d85e
rmgpanw <rmgpanw@ucl.ac.uk>
2022-06-07 14:18:41 +0100

    update get_ukb_synthetic_data function

--- R/ukb_synthetic_dataset.R
+++ R/ukb_synthetic_dataset.R
@@ -11,46 +11,116 @@ UKB_SYNTHETIC_DATASET_DETAILS <- tibble::tribble(
   'bulk.md5',  ""
 )
 
-# PRIVATE -----------------------------------------------------------------
 
-get_ukb_synthetic_data <- function(dataset = "tabular.md5",
-                                   nrows = 10) {
+# PUBLIC ------------------------------------------------------------------
+
+#' Get URL links to from UKB synthetic dataset webpage
+#'
+#' @param pattern Optionally filter the list of URLs for a subset matching a
+#'   pattern.
+#'
+#' @return A character vector of URLs.
+#' @export
+#' @examples
+#' \dontrun{
+#' get_ukb_synthetic_dataset_html_links()
+#'}
+get_ukb_synthetic_dataset_html_links <- function(pattern = "md5$") {
+  # get all urls on the ukb synthetic dataset web page
+  result <- rvest::read_html(
+    "https://biobank.ndph.ox.ac.uk/ukb/exinfo.cgi?src=UKB_Synthetic_Dataset.html"
+  ) %>%
+    rvest::html_elements("a") %>%
+    rvest::html_attr("href")
+
+  # get a subset of urls on the ukb synthetic dataset web page that match a
+  # pattern
+  if (!is.null(pattern)) {
+    result <- subset(result, stringr::str_detect(result,
+                                                 pattern))
+  }
+
+  return(result)
+}
+
+#' Download the UKB synthetic dataset
+#'
+#' @param dataset Character. The name of one of the `.md5` files on the [UKB
+#'   synthetic data
+#'   webpage](https://biobank.ndph.ox.ac.uk/ukb/exinfo.cgi?src=UKB_Synthetic_Dataset.html).
+#'   All
+#' @param download_dir Directory to download files to.
+#'
+#' @return Invisibly returns a list with downloaded file paths and errors for
+#'   failed downloads.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  get_ukb_synthetic_data(download_dir = tempdir())
+#' }
+get_ukb_synthetic_data <- function(
+  download_dir,
+  dataset = "tabular.md5") {
   # validate args
   match.arg(dataset,
             choices = UKB_SYNTHETIC_DATASET_DETAILS$md5)
+
+  if (!fs::dir_exists(download_dir)) {
+    stop("`download_dir` either does not exist or is not a directory. Value supplied:",
+         download_dir)
+  }
 
   start_time <- proc.time()
 
   # make index df
   ukb_dummy_index_df <- make_ukb_dummy_index_df()
+
+  # add download file paths
+  ukb_dummy_index_df$path <- file.path(download_dir,
+                                                ukb_dummy_index_df$file_name)
+
+  # split by dataset
   ukb_dummy_index_df <- split(ukb_dummy_index_df, ukb_dummy_index_df$md5_file)
 
-  # read files
-  result <- fread_ukb_dummy_files(urls = ukb_dummy_index_df[[dataset]]$url,
-                                  fread_fn = purrr::partial(data.table::fread,
-                                                            nrows = nrows))
+  # download files
+  download_file_safely <- purrr::safely(ukbwranglr:::download_file)
+
+  result <- ukb_dummy_index_df[[dataset]][c('download_url', 'path')] %>%
+    furrr::future_pmap(ukbwranglr:::download_file)
+
   ukbwranglr:::time_taken_message(start_time)
 
-  return(result)
+  invisible(result)
 }
+
+# PRIVATE -----------------------------------------------------------------
 
 make_ukb_dummy_index_df <- function() {
   # md5 files
-  md5_files <- get_ukb_dummy_html_links_matching("md5$") %>%
+  md5_files <- get_ukb_synthetic_dataset_html_links(pattern = "md5$") %>%
     fread_ukb_dummy_files(purrr::partial(data.table::fread, header = FALSE)) %>%
     .$result %>%
     dplyr::bind_rows(.id = "md5")
 
   names(md5_files) <- c("md5_file", "md5", "file_name")
 
+  # tempfix - files listed in `rand_chr.md5` should be suffixed with '.gz'
+  md5_files <- md5_files %>%
+    dplyr::mutate("file_name" = ifelse(stringr::str_detect(.data[["file_name"]],
+                                                    "\\.dat$"),
+                                yes = paste0(.data[["file_name"]], ".gz"),
+                                no = .data[["file_name"]]))
+
   # all urls
-  all_urls <- tibble::tibble(url = get_all_ukb_dummy_html_links()) %>%
-    dplyr::mutate("file_name" = installr::file.name.from.url(.data[["url"]]))
+  all_urls <- tibble::tibble(download_url = get_ukb_synthetic_dataset_html_links(pattern = NULL)) %>%
+    dplyr::mutate("file_name" = installr::file.name.from.url(.data[["download_url"]]))
 
   # combine
   result <- md5_files %>%
     dplyr::left_join(all_urls,
-                     by = "file_name")
+                     by = "file_name") %>%
+    tibble::as_tibble()
 
   return(result)
 }
@@ -79,25 +149,4 @@ fread_ukb_dummy_files <- function(urls,
 
   # return results
   return(result)
-}
-
-get_ukb_dummy_html_links_matching <- function(pattern = "md5$") {
-  # get a subset of urls on the ukb synthetic dataset web page that match a
-  # pattern
-  result <- get_all_ukb_dummy_html_links()
-
-  result <- subset(result, stringr::str_detect(result,
-                                               pattern))
-
-
-  return(result)
-}
-
-get_all_ukb_dummy_html_links <- function() {
-  # get all urls on the ukb synthetic dataset web page
-  rvest::read_html(
-    "https://biobank.ndph.ox.ac.uk/ukb/exinfo.cgi?src=UKB_Synthetic_Dataset.html"
-  ) %>%
-    rvest::html_elements("a") %>%
-    rvest::html_attr("href")
 }
